0741b3e61a15758f630653ca43cd747b3d9b0a29
Karl Sixten Bjarnø <karlsixten@gmail.com>
2025-03-18 19:49:58 +0100

    Added location data to notes. City and street is shown on each note

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
 
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Pressable, FlatList } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { useCollection } from 'react-firebase-hooks/firestore'
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, GeoPoint } from 'firebase/firestore';
 import { db } from './firebaseConfig.js';
 
 const Stack = createNativeStackNavigator();
@@ -13,19 +14,60 @@ const Stack = createNativeStackNavigator();
 const firestoreName = 'notes';
 
 export default function App() {
-  const [values, loading, error] = useCollection(collection(db, firestoreName));
+  const [notes, loading, error] = useCollection(collection(db, firestoreName));
   const [inputText, setInputText] = useState("");
 
   async function addNote(inputText) {
+    const noteData = { text: inputText };
+
+    const location = await getLocation();
+    if (location) {
+      const geoPoint = new GeoPoint(location.coords.latitude, location.coords.longitude);
+      const address = await getAddress(geoPoint);
+
+      noteData.locationGeoPoint = geoPoint;
+      noteData.address = address;
+    }
+
     try {
-      await addDoc(collection(db, firestoreName), {
-        text: inputText
-      });
+      console.log(noteData)
+      await addDoc(collection(db, firestoreName), noteData);
       setInputText("");
-    } catch (error){
+    } catch (error) {
       console.log("Error adding note: ", error);
     }
   }
+
+  async function getLocation() {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return null;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+
+    return location;
+  };
+
+  async function getAddress(geoPoint) {
+    try {
+      const addressFromGeoPoint = await Location.reverseGeocodeAsync({ latitude: geoPoint.latitude, longitude: geoPoint.longitude });
+
+      if (addressFromGeoPoint && addressFromGeoPoint.length > 0) {
+        const { city, street } = addressFromGeoPoint[0];
+        const address = { city, street };
+        return address;
+      } else {
+        console.log('No address found for the given coordinates');
+        return undefined;
+      }
+    } catch (error) {
+      console.log('Failed to get address', error)
+    }
+  }
+
+
 
   return (
     <View style={styles.container}>
@@ -39,14 +81,56 @@ export default function App() {
       <Pressable style={styles.saveButton} onPress={() => addNote(inputText)}>
         <Text style={styles.saveButtonText}>Save</Text>
       </Pressable>
+
+      <FlatList
+        data={notes?.docs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.data().text}</Text>
+            {item.data().address && (
+              <Text style={styles.locationText}>
+                {item.data().address.city}, {item.data().address.street}
+              </Text>
+            )}
+          </View>
+        )}
+      />
     </View>
-)}
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    marginTop: 50,
   },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+  saveButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  noteItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    fontSize: 16,
+  },
+  noteText: {
+    fontSize: 20
+  }
 });
